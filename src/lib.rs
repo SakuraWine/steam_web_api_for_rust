adeaa0356031f801906de2a0b21c5647a3651c2b
Sakura Wine <sakura01n@gmail.com>
2024-10-30 21:28:09 +0900

    いろいろ実装

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ mod response;
 
 pub struct SteamAPI {
     client: reqwest::blocking::Client,
+    api_key: String,
 }
 
 
@@ -16,15 +17,15 @@ impl SteamAPI {
     const URL_API_BASE: &'static str =  "http://api.steampowered.com";
     const MAX_LENGTH_NEWS: i32 = 300;
 
-    fn create_base_url(&self, interface: &str, method: &str, version: &str) -> String {
-        format!("{}/{}/{}/v{}/", SteamAPI::URL_API_BASE, interface, method, version)
-    }
-
-    pub fn new() -> Result<SteamAPI, Box<dyn Error>> {
+    pub fn new(api_key: String) -> Result<SteamAPI, Box<dyn Error>> {
         let client = reqwest::blocking::ClientBuilder::new()
             .timeout(Duration::from_secs(10))
             .build()?;
-        Ok(SteamAPI{client})
+        Ok(SteamAPI{client, api_key})
+    }
+
+    fn create_base_url(&self, interface: &str, method: &str, version: &str) -> String {
+        format!("{}/{}/{}/v{}/", SteamAPI::URL_API_BASE, interface, method, version)
     }
 
     pub fn get_news_for_app(&self, app_id: i32, count: i32) -> Result<Vec<NewsItem>, Box<dyn Error>> {
@@ -64,19 +65,47 @@ impl SteamAPI {
             }
         }
     }
+
+    pub fn get_player_summaries(&self, steam_id: i64) {
+        let base_url = self.create_base_url("ISteamUser", "GetPlayerSummaries", "0002");
+        let param_url = format!("?key={}&steamids={}", self.api_key, steam_id);
+        let url = format!("{}{}", base_url, param_url);
+        println!("{url}");
+        let response = self.client.get(url)
+            .send()
+            .unwrap();
+        if response.status().is_success() {
+            println!("Getting player summaries successful.");
+        }
+        println!("{response:?}")
+    }
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::env;
+
     const ID_ENDER_LILIES: i32 = 1369630;
     const ID_ENDER_MAGNOLIA: i32 = 2725260;
+    const MY_PLAYER_ID: i64 = 76561198065075223;
+
+    fn parse_arg() -> Result<String, Box<dyn Error>>{
+        let api_key = env::args().nth(2).unwrap_or("".to_string());
+        Ok(api_key)
+    }
+
+    fn create_api() -> Result<SteamAPI, Box<dyn Error>> {
+        let api_key = parse_arg().unwrap();
+        let api = SteamAPI::new(api_key).unwrap();
+        Ok(api)
+    }
 
     #[test]
     fn get_news() {
         // create API
-        let api = SteamAPI::new().unwrap();
+        let api = create_api().unwrap();
         let news = api.get_news_for_app(ID_ENDER_LILIES, 10);
         assert!(news.is_ok());
         println!("get news test have passed.");
@@ -85,7 +114,7 @@ mod tests {
     #[test]
     fn get_achievements() {
         // create API
-        let api = SteamAPI::new().unwrap();
+        let api = create_api().unwrap();
         // for game which have achievements.
         let achievements = api.get_global_achievement_percentages_for_app(ID_ENDER_LILIES);
         assert!(achievements.is_ok());
@@ -95,5 +124,12 @@ mod tests {
         let achievements = api.get_global_achievement_percentages_for_app(ID_ENDER_MAGNOLIA);
         assert!(achievements.is_ok());
         println!("get achievements test have passed.");
+    }
+
+    #[test]
+    fn get_player_summaries() {
+        // create API
+        let api = create_api().unwrap();
+        api.get_player_summaries(MY_PLAYER_ID);
     }
 }
